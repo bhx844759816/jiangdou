7551004407e3babd2f987b28b32318f00e81802e
bhx844759816 <2466921206@qq.com>
2019-09-03 23:23:49 +0800

    订单界面

--- app/src/main/java/com/jxqm/jiangdou/ui/employer/adapter/WaitPublishJobAdapter.kt
+++ app/src/main/java/com/jxqm/jiangdou/ui/employer/adapter/WaitPublishJobAdapter.kt
@@ -1,11 +1,15 @@
 package com.jxqm.jiangdou.ui.employer.adapter
 
 import android.content.Context
+import android.widget.TextView
 import com.bhx.common.adapter.rv.MultiItemTypeAdapter
 import com.bhx.common.adapter.rv.base.ItemViewType
 import com.bhx.common.adapter.rv.holder.ViewHolder
 import com.jxqm.jiangdou.R
 import com.jxqm.jiangdou.model.WaitPublishJobItem
+import com.jxqm.jiangdou.ui.order.view.OrderPaymentActivity
+import com.jxqm.jiangdou.utils.clickWithTrigger
+import com.jxqm.jiangdou.utils.startActivity
 
 /**
  * 雇主 - 等待发布
@@ -21,6 +25,10 @@ class WaitPublishJobAdapter(context: Context) : MultiItemTypeAdapter<WaitPublish
             override fun isViewForType(item: WaitPublishJobItem?, position: Int): Boolean = true
 
             override fun convert(holder: ViewHolder?, t: WaitPublishJobItem?, position: Int) {
+                val tvAccept = holder?.getView<TextView>(R.id.tvAccept)
+                tvAccept?.clickWithTrigger {
+                    mContext.startActivity<OrderPaymentActivity>()
+                }
             }
         })
     }
---
+++ app/src/main/java/com/jxqm/jiangdou/ui/order/view/OrderPaymentActivity.kt
@@ -0,0 +1,12 @@
+package com.jxqm.jiangdou.ui.order.view
+
+import com.bhx.common.base.BaseActivity
+import com.jxqm.jiangdou.R
+
+/**
+ * 订单支付界面
+ * Created by Administrator on 2019/9/3.
+ */
+class OrderPaymentActivity : BaseActivity() {
+    override fun getLayoutId(): Int = R.layout.activity_order_payment
+}
