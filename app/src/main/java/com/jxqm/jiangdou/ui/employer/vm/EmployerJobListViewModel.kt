076e405093e4923f6134a7bcc05178d23e340168
bhx844759816 <2466921206@qq.com>
2019-09-17 22:15:52 +0800

    认证和发布2

---
+++ app/src/main/java/com/jxqm/jiangdou/ui/employer/vm/EmployerJobListViewModel.kt
@@ -0,0 +1,10 @@
+package com.jxqm.jiangdou.ui.employer.vm
+
+import com.bhx.common.mvvm.BaseViewModel
+import com.jxqm.jiangdou.ui.employer.vm.repository.EmployerJobListRepository
+
+/**
+ * Created by Administrator on 2019/9/17.
+ */
+class EmployerJobListViewModel : BaseViewModel<EmployerJobListRepository>() {
+}
---
+++ app/src/main/java/com/jxqm/jiangdou/ui/employer/vm/repository/EmployerJobListRepository.kt
@@ -0,0 +1,10 @@
+package com.jxqm.jiangdou.ui.employer.vm.repository
+
+import com.jxqm.jiangdou.http.BaseEventRepository
+
+/**
+ *
+ * Created by Administrator on 2019/9/17.
+ */
+class EmployerJobListRepository : BaseEventRepository() {
+}
